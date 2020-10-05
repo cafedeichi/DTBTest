67fabbfb05197cf21a3c2ba05a97e86159268cc6
cafedeichi <ichiro.hirata.biz@gmail.com>
2020-10-05 21:52:57 +0900

    Initial commit

---
+++ DTB/DTB/DTBApp.swift
@@ -0,0 +1,17 @@
+//
+//  DTBApp.swift
+//  DTB
+//
+//  Created by hirata.ichiro on 2020/09/07.
+//
+
+import SwiftUI
+
+@main
+struct DTBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
